bb992f81b8d390d1d6cc39cecff03b818a8ed648
unknown <0306hero@gmail.com>
2021-11-25 17:56:10 -0300

    fix: add options const to avatar command

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -1,6 +1,6 @@
 import BaseCommand from "../abstracts/BaseCommand";
 import Bot from "../bot";
-import { Message } from "discord.js";
+import { ImageURLOptions, Message } from "discord.js";
 
 export default class AvatarCommand extends BaseCommand {
   constructor() {
@@ -10,28 +10,25 @@ export default class AvatarCommand extends BaseCommand {
   async execute(bot: Bot, message: Message, args: string[]): Promise<void> {
     const [original] = args;
 
+    const options: ImageURLOptions = {
+      dynamic: true,
+      size: 4096,
+    };
+
     function returnUserAvatar() {
       if (message.mentions.members.first()) {
         if (message.content.includes("original")) {
-          return message.mentions.users.first().displayAvatarURL({
-            dynamic: true,
-          });
+          return message.mentions.users.first().displayAvatarURL(options);
         }
 
-        return message.mentions.members.first().displayAvatarURL({
-          dynamic: true,
-        });
+        return message.mentions.members.first().displayAvatarURL(options);
       }
 
       if (original) {
-        return message.author.displayAvatarURL({
-          dynamic: true,
-        });
+        return message.author.displayAvatarURL(options);
       }
 
-      return message.member.displayAvatarURL({
-        dynamic: true,
-      });
+      return message.member.displayAvatarURL(options);
     }
 
     message.channel.send(returnUserAvatar());
